3a9ade8d73c47121206bbe77ffe8519a0e48b510
Yatin Sarbalia <yatinsns@gmail.com>
2011-10-07 10:41:27 +0530

    Initial Commit

---
+++ ChatScreenBubble/ChatScreenBubbleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ChatScreenBubbleAppDelegate.h
+//  ChatScreenBubble
+//
+//  Created by Yatin Sarbalia on 07/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatScreenBubbleAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ ChatScreenBubble/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ChatScreenBubble
+//
+//  Created by Yatin Sarbalia on 07/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
